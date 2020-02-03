0052852f213e4774d837b0bc9acacec6dad2f0da
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-02-02 21:11:33 -0800

    Optional country selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,25 @@ $(document).ready(function() {
   // "166a433c57516f51dfab1f7edaed8413" api key
   const API_KEY = "166a433c57516f51dfab1f7edaed8413";
 
+  // Build country selection from country.js
+  COUNTRY.forEach(item => {
+    let option = $("<option>");
+    option.attr("value", item.code);
+    option.text(item.name);
+    $("#country-select").append(option);
+  });
 
+  
   let d = "12/26/1991";
   let temp = 76.3;
   let humidity = 42;
 
-  $("#test").append(weatherCard(d,temp,humidity));
+  $("#test1").append(weatherCard(d,temp,humidity));
+  $("#test2").append(weatherCard(d,temp,humidity));
+  $("#test3").append(weatherCard(d,temp,humidity));
+  $("#test4").append(weatherCard(d,temp,humidity));
+  $("#test5").append(weatherCard(d,temp,humidity));
+  $("#test6").append(weatherCard(d,temp,humidity));
 
 
   /**************************************** Find City Function ****************************************/
@@ -15,21 +28,27 @@ $(document).ready(function() {
   // searchInput = string
   function findCity(searchInput) {
     if (searchInput != "") {
+      if($("#country-select").val() != "Country (Optional)") {
+        searchInput += `, ${$("#country-select").val()}`;
+      }
+
       let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${API_KEY}`;
 
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(res) {
-        console.log(res.cod);
+        console.log(res);
         // cod 200 means that the search is a hit
         if(res.cod === 200) {
-          $("#search-input").removeClass("border border-danger");
+          $("#search").removeClass("border border-danger");
           $("#search-input-error").addClass("d-none");
         }
       }, function(res) {
-        $("#search-input").addClass("border border-danger");
+        $("#search").addClass("border border-danger");
         $("#search-input-error").removeClass("d-none");
+        $("#search-input").val("");
+        $("#country-select").val("Country (Optional)");
       });
     }
   }
@@ -44,12 +63,17 @@ $(document).ready(function() {
 
 
     let cardBody = $("<div>").addClass("card-body");
-    
-    let cardTitle = $("<div>").addClass("card-title");
-    cardBody.append(cardTitle.text(`Temp: ${temp}°F`));
 
-    let cardText = $("<div>").addClass("card-text");
-    cardBody.append(cardText.text(`Humidity: ${humidity}%`));
+    let cardIcon = $("<div>").addClass("card-title");
+    let faIcon = $("<i>").addClass("fas fa-cloud-showers-heavy fa-5x fa-fw");
+    cardBody.append(cardIcon.append(faIcon));
+    
+    
+    let cardTemp = $("<div>").addClass("card-text");
+    cardBody.append(cardTemp.text(`Temp: ${temp}°F`));
+
+    let cardHumidity = $("<div>").addClass("card-text");
+    cardBody.append(cardHumidity.text(`Humidity: ${humidity}%`));
 
     card.append(cardBody);
 
@@ -76,3 +100,5 @@ $(document).ready(function() {
     $(this).addClass("active");
   });
 });
+
+
