e7b559efeac23750b5b3bd472a28c21bbebbf8cf
Hyuk Jun Kwon <hyukjunkwon91@gmail.com>
2020-02-03 20:15:29 -0800

    functionality done

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,44 +10,55 @@ $(document).ready(function() {
     $("#country-select").append(option);
   });
 
+  // Default page = New York City
+  makeQuery("New York City");
 
-  /**************************************** Find City Function ****************************************/
+  /********************************************************* AJAX Query Function *********************************************************/
 
-  // searchInput = string
-  function findCity(searchInput) {
+  function makeQuery(searchInput) {
     if (searchInput != "") {
-      if($("#country-select").val() != "Country (Optional)") {
+      if ($("#country-select").val() != "Country (Optional)") {
         searchInput += `, ${$("#country-select").val()}`;
       }
 
-      let todayQueryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${API_KEY}`;
-      let sixdayQueryURL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${searchInput}&cnt=7&appid=${API_KEY}`;
-      $.ajax({
-        url: sixdayQueryURL,
-        method: "GET"
-      }).then(function(res) {
-        console.log(res);
+      let queryURL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${searchInput}&cnt=7&appid=${API_KEY}`;
 
-        addToday(res);
-        addSixDay(res);
-        // cod 200 means that the search is a hit
-        if(res.cod === 200) {
-          $("#search").removeClass("border border-danger");
-          $("#search-input-error").addClass("d-none");
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(
+        function(res) {
+          addToday(res);
+          addSixDay(res);
+          addSearchResult(res);
+
+          if (res.cod === 200) {
+            $("#search").removeClass("border border-danger");
+            $("#search-input-error").addClass("d-none");
+          }
+        },
+        function(res) {
+          $("#search").addClass("border border-danger");
+          $("#search-input-error").removeClass("d-none");
+          $("#search-input").val("");
+          $("#country-select").val("Country (Optional)");
         }
-      }, function(res) {
-        $("#search").addClass("border border-danger");
-        $("#search-input-error").removeClass("d-none");
-        $("#search-input").val("");
-        $("#country-select").val("Country (Optional)");
-      });
+      );
     }
   }
 
+  /*********************************************************** Display Forecasts ***********************************************************/
+
   function addToday(res) {
+    $("#today-forecast").empty();
+
     // Add Today's weather
     let todayTitle = $("<h4>");
-    todayTitle.text(`${res.city.name}, ${res.city.country} (${moment(res.list[0].dt * 1000).format("MM/DD/YYYY h:MM A")})`)
+    todayTitle.text(
+      `${res.city.name}, ${res.city.country} (${moment(
+        res.list[0].dt * 1000
+      ).format("MM/DD/YYYY")})`
+    );
     $("#today-forecast").append(todayTitle);
 
     let todayTemp = $("<p>").addClass("lead");
@@ -61,39 +72,53 @@ $(document).ready(function() {
     let todayWind = $("<p>").addClass("lead");
     todayWind.text(`Wind Speed: ${res.list[0].speed} MPH`);
     $("#today-forecast").append(todayWind);
-
   }
 
   function addSixDay(res) {
-    console.log(res);
     // Add city name and country
-    $("#6day-forecast").text(`${res.city.name}, ${res.city.country} `);
+    $("#6day-forecast").html(`${res.city.name}, ${res.city.country} `);
 
-    for(let i = 1; i < 7; i++) {
+    for (let i = 1; i < 7; i++) {
       let d = moment(res.list[i].dt * 1000).format("MM/DD/YYYY");
       let t = toF(res.list[i].temp.day);
       let h = res.list[i].humidity;
-      $(`#day${i}`).append(weatherCard(d,t,h))
+      $(`#day${i}`).html(weatherCard(d, t, h));
     }
   }
 
-  function toF(Kel) {
-    return Number((Kel - 273.15) * 1.8 + 32).toFixed(1);
-  }
+  /******************************************************** City list Manipulation ********************************************************/
+  function addSearchResult(res) {
+    let name = res.city.name;
+    let code = res.city.country;
 
-  function addSearchResult(name, code) {
-    // ************************ If the city is already on the list?
+    for (let i = 0; i < $(".list-group-item").length; i++) {
+      if (
+        $(".list-group-item")[i].getAttribute("id") === name &&
+        $(".list-group-item")[i].getAttribute("data-country") === code
+      ) {
+        $(".active").removeClass("active");
+        $(`.list-group-item[id="${name}"]`).addClass("active");
+        return;
+      }
+    }
 
     // Remove active attribute from the current city
     $(".active").removeClass("active");
 
-
-    let button = $("<button>").addClass("list-group-item list-group-item-action active");
-    button.attr("data-city", name);
+    let button = $("<button>").addClass(
+      "list-group-item list-group-item-action active"
+    );
+    button.attr("id", name);
     button.attr("data-country", code);
     button.text(`${name}, ${code}`);
 
     $("#city-list").append(button);
+  }
+
+  /********************************************************* Utility Functions *********************************************************/
+
+  function toF(Kel) {
+    return Number((Kel - 273.15) * 1.8 + 32).toFixed(1);
   }
 
   function weatherCard(date, temp, humidity) {
@@ -102,14 +127,12 @@ $(document).ready(function() {
     let cardHeader = $("<div>").addClass("card-header bg-primary text-white");
     card.append(cardHeader.text(date));
 
-
     let cardBody = $("<div>").addClass("card-body");
 
     let cardIcon = $("<div>").addClass("card-title");
     let faIcon = $("<i>").addClass("fas fa-cloud-showers-heavy fa-3x");
     cardBody.append(cardIcon.append(faIcon));
-    
-    
+
     let cardTemp = $("<div>").addClass("card-text");
     cardBody.append(cardTemp.text(`Temp: ${temp}°F`));
 
@@ -121,25 +144,32 @@ $(document).ready(function() {
     return card;
   }
 
-  /******************************************* Event Listeners *******************************************/
+  /********************************************************* Event Listeners *********************************************************/
   $("#find-city").on("click", function(event) {
     event.preventDefault();
 
-    let searchInput = $("#search-input").val();
-    findCity(searchInput);
+    if ($("#search-input").val() != "") {
+      let searchInput = $("#search-input").val();
+      if ($("#country-select").val() != "Country (Optional)") {
+        searchInput += `, ${$("#country-select").val()}`;
+      }
+      makeQuery(searchInput);
+    }
   });
 
   $(document).on("keydown", function(event) {
-    if (event.which === 13 && $("#search-input").val() != ""){
-      findCity($("#search-input").val());
+    if (event.which === 13 && $("#search-input").val() != "") {
+      let searchInput = $("#search-input").val();
+      if ($("#country-select").val() != "Country (Optional)") {
+        searchInput += `, ${$("#country-select").val()}`;
+      }
+      makeQuery(searchInput);
     }
   });
 
   $(".list-group-item").on("click", function(event) {
-    console.log($(this).attr("data-city"));
     $(".active").removeClass("active");
     $(this).addClass("active");
+    makeQuery(`${$(this).attr("id")}, ${$(this).attr("data-country")}`);
   });
 });
-
-
